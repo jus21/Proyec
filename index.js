1827a2d8ff9c3787d37452bbf23a7de5602584d2
jus21 <141685797+jus21@users.noreply.github.com>
2023-08-07 21:18:41 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function(){
+    alert('pilas, paso por ti el sábado 16 de septiembre a las 14:00 PM :3')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('click',function(){
+    alert('Acepta oye, te va a gustar .,.')
+});
